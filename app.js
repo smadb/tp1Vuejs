4f86a860895bc438c5cf1c22909c120dda704800
ClemR <116346142+smadb@users.noreply.github.com>
2023-03-28 15:52:22 +0200

    nb favoris ajout texte

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ Vue.createApp({
          valeurNom:'Julien',
          valeurAge:'45 ans',
          valeurAgePlus:'55 ans, 10 ans plus tard',
-         nbRandom:0,
+         nbRandom:'',
 
          valeurDeNom:'',
          valeurDeAge:'',
@@ -18,7 +18,7 @@ Vue.createApp({
             this.valeurNom = `${this.valeurDeNom}`;
             this.valeurAge = `${this.valeurDeAge} ans`;
             this.valeurAgePlus = `${parseInt(this.valeurDeAge) + 10} ans, 10 ans plus tard`;
-            this.nbRandom=Math.random();
+            this.nbRandom=`Nb Favoris : ${Math.random()}`;
         }
        
     },
